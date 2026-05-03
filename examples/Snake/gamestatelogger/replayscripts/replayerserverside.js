f5668d68b590062b5216f941e6d619999007be76
tomat-suppe <slak@itu.dk>
2026-05-03 17:28:17 +0200

    FEATURE: Server-side replaying can now read multiple files from at directory for replaying.

--- examples/Snake/gamestatelogger/replayscripts/replayerserverside.js
+++ examples/Snake/gamestatelogger/replayscripts/replayerserverside.js
@@ -1,3 +1,4 @@
+import fs from 'fs';
 class Snake {
     constructor() {
         this.canvasW = 400;
@@ -7,9 +8,9 @@ class Snake {
         /* Below just for testing to confirm i and frameCount are synced.
         Matches timeStep from main game. */
         this.frameCount = 0;
-
-        this.canvas = document.getElementById('game');
-        this.context = this.canvas.getContext('2d');
+        /* Visual components out-commented for running in command line */
+        /*this.canvas = document.getElementById('game');
+        this.context = this.canvas.getContext('2d');*/
     }
 
     snake = {
@@ -22,7 +23,6 @@ class Snake {
 
         // keep track of all grids the snake body occupies
         cells: [],
-
         // length of the snake. grows when eating an apple
         maxCells: 4
     };
@@ -38,7 +38,7 @@ class Snake {
         var answer = "";
         this.snake.cells.forEach((cell, index) => {
             for (let i = index + 1; i < this.snake.cells.length; i++) {
-                if (/*this.snake.cells[0]*/cell.x === this.snake.cells[i].x && /*this.snake.cells[0]*/cell.y === this.snake.cells[i].y) {
+                if (cell.x === this.snake.cells[i].x && cell.y === this.snake.cells[i].y) {
                     this.snake.x = 160;
                     this.snake.y = 160;
 
@@ -48,7 +48,6 @@ class Snake {
         });
         return answer;
     }
-    
 
     mainLoop(){
         if (++this.count < 16) {
@@ -56,8 +55,9 @@ class Snake {
             return;
         }
         this.count = 0;
-
-        this.context.clearRect(0,0, this.canvas.width, this.canvas.height);
+        
+        /* Visual stuff out-commented for compliance with running in command line
+        this.context.clearRect(0,0, this.canvas.width, this.canvas.height);*/
         
         this.snake.x += this.snake.dx;
         this.snake.y += this.snake.dy;
@@ -82,6 +82,7 @@ class Snake {
             this.snake.cells.pop();
         }
 
+        /* Visual stuff out-commented for compliance with running in command line
         // draw apple
         this.context.fillStyle = 'red';
         this.context.fillRect(this.apple.x, this.apple.y, this.grid-1, this.grid-1);
@@ -92,7 +93,7 @@ class Snake {
         this.snake.cells.forEach(cell => {
             // drawing 1 px smaller than the grid creates a grid effect in the snake body so you can see how long it is
             this.context.fillRect(cell.x, cell.y, this.grid-1, this.grid-1);
-        });
+        });*/
 
         this.frameCount++;
     }
@@ -101,25 +102,22 @@ class Snake {
         if (e === 37 && this.snake.dx === 0) {
             this.snake.dx = -this.grid;
             this.snake.dy = 0;
-            console.log("Moved snake left.");
+            
         }
-        // up arrow key
         else if (e === 38 && this.snake.dy === 0) {
             this.snake.dy = -this.grid;
             this.snake.dx = 0;
-            console.log("Moved snake up.");
+            
         }
-        // right arrow key
         else if (e === 39 && this.snake.dx === 0) {
             this.snake.dx = this.grid;
             this.snake.dy = 0;
-            console.log("Moved snake right.");
+
         }
-        // down arrow key
         else if (e === 40 && this.snake.dy === 0) {
             this.snake.dy = this.grid;
             this.snake.dx = 0;
-            console.log("Moved snake down.");
+
         }
         this.frameCount++;
     }
@@ -129,100 +127,87 @@ class Snake {
 var valid = false;
 
 let gameInstance = new Snake();
+const dir = "../server/JSONfiles/";
 
-let GAMEDATA =
-[{"ID":"./JSONfiles/mopq0vr80.ba50x386oz.json","eventName":"keyDown","keyPressed":40,"eventTime":952,"points":4},
-    {"ID":"./JSONfiles/mopq0vr80.ba50x386oz.json","level":"Ate apple!","eventTime":1098,"points":5},
-    {"ID":"./JSONfiles/mopq0vr80.ba50x386oz.json","eventName":"Random seed: Apple x","randomSeed":256,"eventTime":1098},
-    {"ID":"./JSONfiles/mopq0vr80.ba50x386oz.json","eventName":"Random seed: Apple y","randomSeed":96,"eventTime":1098},
-    {"ID":"./JSONfiles/mopq0vr80.ba50x386oz.json","eventName":"keyDown","keyPressed":37,"eventTime":1100,"points":5},
-    {"ID":"./JSONfiles/mopq0vr80.ba50x386oz.json","eventName":"keyDown","keyPressed":38,"eventTime":1161,"points":5},
-    {"ID":"./JSONfiles/mopq0vr80.ba50x386oz.json","eventName":"keyDown","keyPressed":37,"eventTime":1377,"points":5},
-    {"ID":"./JSONfiles/mopq0vr80.ba50x386oz.json","level":"Ate apple!","eventTime":1389,"points":6},
-    {"ID":"./JSONfiles/mopq0vr80.ba50x386oz.json","eventName":"Random seed: Apple x","randomSeed":80,"eventTime":1389},
-    {"ID":"./JSONfiles/mopq0vr80.ba50x386oz.json","eventName":"Random seed: Apple y","randomSeed":352,"eventTime":1389},
-    {"ID":"./JSONfiles/mopq0vr80.ba50x386oz.json","eventName":"keyDown","keyPressed":40,"eventTime":1397,"points":6},
-    {"ID":"./JSONfiles/mopq0vr80.ba50x386oz.json","eventName":"keyDown","keyPressed":39,"eventTime":1419,"points":6},
-    {"ID":"./JSONfiles/mopq0vr80.ba50x386oz.json","gameEnd":true,"eventName":"Game over!","eventTime":1423,"points":6,"highscore":"n/a"}];
+let filesArr = fs.readdirSync(dir);
+console.log("Files in directory: " + filesArr);
+let noOfFiles = 10;
 
-var maxTime = GAMEDATA[GAMEDATA.length - 1].eventTime;
+for(var j = 0; j <= noOfFiles; j++) {
+    console.log(`Replaying file no. ${j}, ID: ${filesArr[j]}`);
+    let path = dir + filesArr[j];
+    let GAMEDATA = JSON.parse(fs.readFileSync(path));
 
-var element = GAMEDATA[0];
-var nextElement = 1;
+    var maxTime = GAMEDATA[GAMEDATA.length - 1].eventTime;
 
-//var i = 0;
-//const inter = setInterval(function() {
-for(var i = 0; i <= maxTime; i++) {
-    /* Below just for testing, ensuring the i loop matches the frameCount
-       which corresponds to timeSkip, logical time from main game.*/
-    console.log(`i is ${i} and frameCount is ${gameInstance.frameCount}`);
-    if(i === element.eventTime){
-        if (element.eventTime === maxTime) {
-            /* Run main loop before checking for death, as the check usually
-               happens at the end of the main loop. */
-            gameInstance.mainLoop();
-            valid = gameInstance.collisionSnake() === "Dead";
-            console.log("Was game run valid?: " + valid);
-            break;
-            //clearInterval(inter);
-        }
+    var element = GAMEDATA[0];
+    var nextElement = 1;
 
-        if(element != null && element.level === "Ate apple!"){
-            gameInstance.snake.maxCells++;
-            /* Remember that the game progresses when eating an apple, 
+    for(var i = 0; i <= maxTime; i++) {
+        if(i === element.eventTime){
+            if (element.eventTime === maxTime) {
+                gameInstance.mainLoop();
+                valid = gameInstance.collisionSnake() === "Dead";
+                console.log("Game valid? " + valid);
+                break;
+            }
+
+            if(element != null && element.level === "Ate apple!"){
+                gameInstance.snake.maxCells++;
+                /* Remember that the game progresses when eating an apple, 
                usually happens in the end of the main loop. */
-            element = GAMEDATA[nextElement];
-            nextElement+=1;
-            gameInstance.apple.x = element.randomSeed;
-            // Progress the array to get the y element random seed.
-            element = GAMEDATA[nextElement];
-            nextElement+=1;
-            gameInstance.apple.y = element.randomSeed;
             
+                element = GAMEDATA[nextElement];
+                nextElement+=1;
+                gameInstance.apple.x = element.randomSeed;
+                // Progress the array to get the y element random seed.
+                element = GAMEDATA[nextElement];
+                nextElement+=1;
+                gameInstance.apple.y = element.randomSeed;
+                
+                gameInstance.mainLoop();
+            }
+
+            switch (element.eventName) {
+                case "keyDown":
+                    switch (element.keyPressed) {
+                        case 37:
+                            gameInstance.moveSnake(37);
+                            break;
+                        case 38:
+                            gameInstance.moveSnake(38);
+                            break;
+                        case 39:
+                            gameInstance.moveSnake(39);
+                            break;
+                        case 40:
+                            gameInstance.moveSnake(40);
+                            break;
+                        default:
+                            break;
+                    }
+                default:
+                    break; 
+            }
+
+            element = GAMEDATA[nextElement];
+
+            if (element != null && element.eventTime === i) {
+                i--;
+                // Keeping logical time consistent.
+                gameInstance.frameCount--;
+            }
+
+            nextElement+=1; 
+            
+            if (nextElement >= GAMEDATA.length + 1) {
+                element = null;
+            }
+        } else {
+            /* As moves are independent of the main loop, main loop is only
+           executed when a move is not. */
+        
             gameInstance.mainLoop();
         }
-
-        switch (element.eventName) {
-            case "keyDown":
-                switch (element.keyPressed) {
-                    case 37:
-                        gameInstance.moveSnake(37);
-                        break;
-                    case 38:
-                        gameInstance.moveSnake(38);
-                        break;
-                    case 39:
-                        gameInstance.moveSnake(39);
-                        break;
-                    case 40:
-                        gameInstance.moveSnake(40);
-                        break;
-                    default:
-                        break;
-                }
-            default:
-                break; 
-        }
-
-        element = GAMEDATA[nextElement];
-
-        if (element != null && element.eventTime === i) {
-            i--;
-            // Keeping logical time consistent.
-            gameInstance.frameCount--;
-        }
-
-        nextElement+=1; 
-        
-        if (nextElement >= GAMEDATA.length + 1) {
-            element = null;
-        }
-    } else {
-        /* As moves are independent of the main loop, main loop is only
-           executed when a move is not. */
-        gameInstance.mainLoop();
     }
 }
-/*    i++;
-}, 10);
-*/
